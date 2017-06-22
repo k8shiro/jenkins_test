23d87a1884e83b4088a207214f71dcb1d6d5d5ad
yatsushiro <yatsushiro@example.com>
2017-06-22 15:31:06 +0900

    fix run.sh

--- run.sh
+++ run.sh
@@ -7,7 +7,7 @@ echo "Ubuntu------------"
 cat /etc/lsb-release
 
 
-docker run -i -t -d --name ansible ansible/centos7-ansible bin
+docker run -i -t -d --name ansible ansible/centos7-ansible /bin/bash
 docker exec ansible ansible --version
 
 
