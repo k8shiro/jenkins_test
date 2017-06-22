1ee84b103326c592038a4cb2cd72cbd633b6469b
yatsushiro <yatsushiro@example.com>
2017-06-22 15:03:50 +0900

    fix run.sh

--- run.sh
+++ run.sh
@@ -9,7 +9,7 @@ cat /etc/lsb-release
 
 docker pull centos:centos7
 
-docker run --privileged -d -ti -e "container=docker"  -v /sys/fs/cgroup:/sys/fs/cgroup -v `pwd`:/htcondor-ce:rw  centos:centos${OS_VERSION}   /usr/sbin/init
+docker run --privileged -d -ti -e "container=docker"  -v /sys/fs/cgroup:/sys/fs/cgroup -v `pwd`:/htcondor-ce:rw  centos:centos7   /usr/sbin/init
 
 
 
