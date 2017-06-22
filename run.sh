05838391a70819c4dcc7117386eacccf1049f577
yatsushiro <yatsushiro@example.com>
2017-06-22 15:00:51 +0900

    fix run.sh

--- run.sh
+++ run.sh
@@ -5,3 +5,12 @@ cat /etc/redhat-release
 
 echo "Ubuntu------------"
 cat /etc/lsb-release
+
+
+docker pull centos:centos7
+
+docker run --privileged -d -ti -e "container=docker"  -v /sys/fs/cgroup:/sys/fs/cgroup -v `pwd`:/htcondor-ce:rw  centos:centos${OS_VERSION}   /usr/sbin/init
+
+
+
+
