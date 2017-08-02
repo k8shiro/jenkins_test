9705beca86126682d31964f3eccf8ec4c70ef073
yatsushiro <yatsushiro@example.com>
2017-08-02 11:07:54 +0900

    test

--- run.sh
+++ run.sh
@@ -1,14 +1,2 @@
 whoami
 
-echo "RHEL--------------"
-cat /etc/redhat-release
-
-echo "Ubuntu------------"
-cat /etc/lsb-release
-
-
-docker run -i -t -d --name ansible ansible/centos7-ansible /bin/bash
-docker exec ansible ansible --version
-
-
-
